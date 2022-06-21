b4e1f0f5f94af51ff9f92816b8aedaebbe394c79
Favour Edubio <104675975+Feke21@users.noreply.github.com>
2022-06-21 18:30:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var Name = " Name:Favour Edubio";
+var Height = "Height:5 ft 6";
+var Country = "Country:Nigeria";
+
+document.getElementById("demo").innerHTML = Name + "<br>" + Height + "<br>" + Country;
